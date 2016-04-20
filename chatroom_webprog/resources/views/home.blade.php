e179a6ca49347ecdaf17e25d433c756f556c23aa
punyapats <evilpun1402@gmail.com>
2016-04-20 21:54:02 +0700

    split page

--- chatroom_webprog/resources/views/home.blade.php
+++ chatroom_webprog/resources/views/home.blade.php
@@ -1,15 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container chatbox">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-10 col-md">    
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
                     You are logged in!
                 </div>
+            </div>
+        </div>
+        <div class="col-md-2 col-md"> 
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                    <div class="panel-body">
+                        You are logged in!
+                    </div>
             </div>
         </div>
     </div>
