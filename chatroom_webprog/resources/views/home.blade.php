e7af0a765cf7c55a889bd7112ec61d012d593016
punyapats <evilpun1402@gmail.com>
2016-04-20 23:10:50 +0700

    chatbox

--- chatroom_webprog/resources/views/home.blade.php
+++ chatroom_webprog/resources/views/home.blade.php
@@ -3,16 +3,20 @@
 @section('content')
 <div class="container chatbox">
     <div class="row">
-        <div class="col-md-10 col-md">    
+        <div class="col-md-8 col-md">    
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">ChatBox</div>
 
-                <div class="panel-body">
-                    You are logged in!
+                <div class="panel-body chatbody">
+                    <div class="chat">
+                        You are logged in!
+                    </div>
+                    {{ Form::text('message') }}
+                    {{ Form::submit('Send', ['class' => 'btn btn-large btn-primary send']) }}
                 </div>
             </div>
         </div>
-        <div class="col-md-2 col-md"> 
+        <div class="col-md-4 col-md"> 
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
