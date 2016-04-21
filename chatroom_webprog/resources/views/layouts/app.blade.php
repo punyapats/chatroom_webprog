596f9f712f2f53eb2d22cee251d7c45005d555f6
Earth <earth_unknow@hotmail.com>
2016-04-22 02:56:57 +0700

    Change Name

--- chatroom_webprog/resources/views/layouts/app.blade.php
+++ chatroom_webprog/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Web Chat</title>
 
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
@@ -42,7 +42,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    WEBCHAT
                 </a>
             </div>
 
