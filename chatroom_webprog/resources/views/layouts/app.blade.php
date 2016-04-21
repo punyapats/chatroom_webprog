474d7f8fde7177bca6f6f1280e20a9bcb251b8c7
Earth <earth_unknow@hotmail.com>
2016-04-21 20:58:31 +0700

    Can logout

--- chatroom_webprog/resources/views/layouts/app.blade.php
+++ chatroom_webprog/resources/views/layouts/app.blade.php
@@ -4,14 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>Laravel</title>
 
-     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-    
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
     <link href="css/style.css" rel='stylesheet' type='text/css'>
@@ -81,10 +76,8 @@
     @yield('content')
 
     <!-- JavaScripts -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="js/chat.js"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-   
 </body>
 </html>
