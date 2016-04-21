eb0f324fdaa10babd73f4061c8376ef5ce9d877e
punyapats <evilpun1402@gmail.com>
2016-04-21 15:43:42 +0700

    show name

--- chatroom_webprog/app/Http/Controllers/HomeController.php
+++ chatroom_webprog/app/Http/Controllers/HomeController.php
@@ -27,7 +27,9 @@ class HomeController extends Controller
     public function index()
     {
         $id = Auth::user()->id;
-        $friendlist = DB::table('flist')->where('user',$id)->get();
+        // $friendlist = DB::table('flist')->where('user',$id)->get();
+        $friendlist = DB::table('flist')->where('user',$id)->join('users','flist.friend','=','users.id')->select('users.name')->get();
+
         // return view('home',compact('friendlist'));
         return view('home',compact('friendlist'));
     }
